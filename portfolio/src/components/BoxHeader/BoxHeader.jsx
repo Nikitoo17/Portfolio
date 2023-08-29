eda1413b2b8e129e8b70f807c06bcb1301e8c8b5
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 16:38:39 -0300

    Update BoxHeader.jsx

--- portfolio/src/components/BoxHeader/BoxHeader.jsx
+++ portfolio/src/components/BoxHeader/BoxHeader.jsx
@@ -1,27 +1,25 @@
 import React from "react";
-import style from "./BoxHeader.module.css";
 import { motion } from "framer-motion";
 const BoxHeader = () => {
   return (
-    // initial={{opacity: 0, y: "60px"}}  transition={{duration:1}} whileInView={{ y: "0px", opacity: 1}}
     <motion.div
-      className={style.divBox}
+      className="bg-opacity-30 bg-white p-6 max-w-[600px] border border-opacity-10 rounded-md text-justify mt-10"
       initial={{ opacity: 0.8, scale: 0.9 }}
       transition={{ duration: 1, delay: 0.1 }}
       whileInView={{ scale: 1, opacity: 1 }}
     >
-      <h3>Sobre Mí</h3>
-      <p>
+      <h3 className="text-center text-2xl mb-2">Sobre Mí</h3>
+      <p className="text-start text-sm mb-2">
         ¡Hola, soy Juan Facundo Tam! Soy un apasionado desarrollador FullStack
         que decidió dar un giro vocacional desde la psicología hacia la
         informática para dedicarme a lo que realmente me apasiona: la
         programación.
       </p>
-      <p>
+      <p className="text-start text-sm mb-2">
         Desde entonces, me he mantenido en constante formación, investigando,
         aprendiendo y acumulando experiencia.
       </p>
-      <p>
+      <p className="text-start text-sm mb-2">
         Me considero una persona responsable, perseverante y creativa, y destaco
         en la resolución de problemas mediante la búsqueda activa de soluciones
         y alternativas. Tengo una gran apertura, interés y predisposición para
