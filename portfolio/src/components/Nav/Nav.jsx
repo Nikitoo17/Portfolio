b9676197065dd6e59d2964691d7534457faead62
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 17:34:04 -0300

    Update Nav.jsx

--- portfolio/src/components/Nav/Nav.jsx
+++ portfolio/src/components/Nav/Nav.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import { motion } from "framer-motion";
 import { IoPersonOutline } from "react-icons/io5";
 import { TfiBriefcase } from "react-icons/tfi";
-import { MdOutlinePhonelinkRing } from "react-icons/md";
-import { BsPhone, BsPersonVcard } from "react-icons/bs";
-import cv from "../../utils/JuanFacundoTamCV.pdf"
+import { BsPersonVcard } from "react-icons/bs";
 
-import { NavLink } from "react-router-dom";
 import style from "./Nav.module.css";
 const Nav = () => {
   return (
     <motion.div
       className={style.divNav}
-      // animate={{y: "20px"}}
       whileInView={{ y: "20px" }}
       transition={{ type: "spring", bounce: 0.6, duration: 1 }}
     >
@@ -27,9 +23,13 @@ const Nav = () => {
       <a href="#proyectos" className={style.link}>
         <TfiBriefcase className={style.icons} />
       </a>
-      <a href="https://github.com/juanfacundotam/juanfacundotam/raw/main/Juan%20Facundo%20Tam%20CV.pdf" className={style.link} download>
-      {/* <BsPhone className={style.icons} /> */}
-      <p>CV</p>
+      <a
+        href="https://github.com/juanfacundotam/juanfacundotam/raw/main/Juan%20Facundo%20Tam%20CV.pdf"
+        className={style.link}
+        download
+      >
+        {/* <BsPhone className={style.icons} /> */}
+        <p>CV</p>
       </a>
     </motion.div>
   );
