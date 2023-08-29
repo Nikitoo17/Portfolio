3d483738144191a7193b5525634a257c0d467117
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 17:33:28 -0300

    Update Home.jsx

--- portfolio/src/views/Home/Home.jsx
+++ portfolio/src/views/Home/Home.jsx
@@ -9,31 +9,34 @@ import Skills from "../../components/Skills/Skills";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-start pt-40 w-full px-40">
+    <div
+      className="flex flex-col items-center justify-start pt-40 w-full px-40"
+      id="home"
+    >
       <div className="flex justify-between w-full items-center">
         <div className="flex flex-col">
           <motion.img
             src={foto}
             alt="Foto Personal"
-            initial={{ opacity: 1, scale: 0.8 }}
+            initial={{ opacity: 0, scale: 0.9 }}
             whileInView={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 1, delay: 0.1 }}
-            className="w-[700px] rounded-[15px]  mt-5"
+            transition={{ duration: 1 }}
+            className="w-[700px] rounded-[15px]  mt-5  shadow-sm shadow-purple-950 mx-auto"
           />
         </div>
         <div className="flex flex-col justify-between h-full">
           <motion.h1
-            initial={{ opacity: 0.8, x: "-20px" }}
-            whileInView={{ x: "0px", opacity: 1 }}
-            transition={{ duration: 1, delay: 0.1 }}
+            initial={{ opacity: 0.8, y: 200 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
             className="text-4xl font-bold"
           >
             Nahuel Nicolas Fadel
           </motion.h1>
           <motion.h2
-            initial={{ opacity: 0.8, x: "20px" }}
-            whileInView={{ x: "0px", opacity: 1 }}
-            transition={{ duration: 1, delay: 0.1 }}
+            initial={{ opacity: 0, y: 200 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
             className="text-xl"
           >
             FullStack Developer
