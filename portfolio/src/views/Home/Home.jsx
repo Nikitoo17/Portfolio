327d42d7b207524e84583d0e829d5c0740e27516
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 16:38:41 -0300

    Update Home.jsx

--- portfolio/src/views/Home/Home.jsx
+++ portfolio/src/views/Home/Home.jsx
@@ -9,33 +9,37 @@ import Skills from "../../components/Skills/Skills";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-start pt-40">
-      <motion.h1
-        initial={{ opacity: 0.8, x: "-20px" }}
-        whileInView={{ x: "0px", opacity: 1 }}
-        transition={{ duration: 1, delay: 0.1 }}
-        className="text-4xl font-bold"
-      >
-        Nahuel Nicolas Fadel
-      </motion.h1>
-      <motion.h2
-        initial={{ opacity: 0.8, x: "20px" }}
-        whileInView={{ x: "0px", opacity: 1 }}
-        transition={{ duration: 1, delay: 0.1 }}
-        className="text-xl"
-      >
-        FullStack Developer
-      </motion.h2>
-      <motion.img
-        src={foto}
-        alt="Foto Personal"
-        initial={{ opacity: 0.8, scale: 0.9 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1, delay: 0.1 }}
-        className="max-w-[270px] rounded-[15px] md:w-60 md:rounded-[40px] mt-5"
-      />
-      <Social />
-      <BoxHeader />
+    <div className="flex flex-col items-center justify-start pt-40 w-full px-40">
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <motion.h1
+            initial={{ opacity: 0.8, x: "-20px" }}
+            whileInView={{ x: "0px", opacity: 1 }}
+            transition={{ duration: 1, delay: 0.1 }}
+            className="text-4xl font-bold"
+          >
+            Nahuel Nicolas Fadel
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0.8, x: "20px" }}
+            whileInView={{ x: "0px", opacity: 1 }}
+            transition={{ duration: 1, delay: 0.1 }}
+            className="text-xl"
+          >
+            FullStack Developer
+          </motion.h2>
+          <motion.img
+            src={foto}
+            alt="Foto Personal"
+            initial={{ opacity: 0.8, scale: 0.9 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.1 }}
+            className="w-[600px] rounded-[15px]  mt-5"
+          />
+          <Social />
+        </div>
+        <BoxHeader />
+      </div>
       <Skills />
     </div>
   );
