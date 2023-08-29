9b9589294664aa73131e31f61ab587d102607a51
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 16:33:19 -0300

    Update Home.jsx

--- portfolio/src/views/Home/Home.jsx
+++ portfolio/src/views/Home/Home.jsx
@@ -1,39 +1,20 @@
 import React from "react";
-import style from "./Home.module.css";
 import foto from "../../assets/foto.jpg";
 import Social from "../../components/Social/Social";
 import BoxHeader from "../../components/BoxHeader/BoxHeader";
 import { motion } from "framer-motion";
-
-import {
-  SiJavascript,
-  SiPostgresql,
-  SiExpress,
-  SiReact,
-  SiRedux,
-  SiCss3,
-  SiHtml5,
-  SiNodedotjs,
-  SiNpm,
-  SiSass,
-  SiLess,
-  SiCplusplus,
-  SiCsharp,
-  SiDotnet,
-  SiLinux,
-} from "react-icons/si";
-
 import Skills from "../../components/Skills/Skills";
 
 // animate={{ x: "0px", opacity: 1}}
 
 const Home = () => {
   return (
-    <div className={style.divHome} id="home">
+    <div className="flex flex-col items-center justify-start pt-40">
       <motion.h1
         initial={{ opacity: 0.8, x: "-20px" }}
         whileInView={{ x: "0px", opacity: 1 }}
         transition={{ duration: 1, delay: 0.1 }}
+        className="text-4xl font-bold"
       >
         Nahuel Nicolas Fadel
       </motion.h1>
@@ -41,16 +22,17 @@ const Home = () => {
         initial={{ opacity: 0.8, x: "20px" }}
         whileInView={{ x: "0px", opacity: 1 }}
         transition={{ duration: 1, delay: 0.1 }}
+        className="text-xl"
       >
         FullStack Developer
       </motion.h2>
       <motion.img
         src={foto}
-        className={style.image}
         alt="Foto Personal"
         initial={{ opacity: 0.8, scale: 0.9 }}
         whileInView={{ scale: 1, opacity: 1 }}
         transition={{ duration: 1, delay: 0.1 }}
+        className="max-w-[270px] rounded-[15px] md:w-60 md:rounded-[40px] mt-5"
       />
       <Social />
       <BoxHeader />
