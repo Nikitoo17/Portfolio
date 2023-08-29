4640e6b8ee979809df1d48b72fcc1f99d97f6fbf
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 16:55:33 -0300

    Update Home.jsx

--- portfolio/src/views/Home/Home.jsx
+++ portfolio/src/views/Home/Home.jsx
@@ -10,8 +10,18 @@ import Skills from "../../components/Skills/Skills";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-start pt-40 w-full px-40">
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full items-center">
         <div className="flex flex-col">
+          <motion.img
+            src={foto}
+            alt="Foto Personal"
+            initial={{ opacity: 1, scale: 0.8 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.1 }}
+            className="w-[700px] rounded-[15px]  mt-5"
+          />
+        </div>
+        <div className="flex flex-col justify-between h-full">
           <motion.h1
             initial={{ opacity: 0.8, x: "-20px" }}
             whileInView={{ x: "0px", opacity: 1 }}
@@ -28,17 +38,9 @@ const Home = () => {
           >
             FullStack Developer
           </motion.h2>
-          <motion.img
-            src={foto}
-            alt="Foto Personal"
-            initial={{ opacity: 0.8, scale: 0.9 }}
-            whileInView={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 1, delay: 0.1 }}
-            className="w-[600px] rounded-[15px]  mt-5"
-          />
+          <BoxHeader />
           <Social />
         </div>
-        <BoxHeader />
       </div>
       <Skills />
     </div>
