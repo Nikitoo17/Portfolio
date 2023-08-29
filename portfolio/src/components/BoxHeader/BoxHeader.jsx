9242583ab3a08ef719dcd3f7f0ccfdaa477629b9
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 16:55:30 -0300

    Update BoxHeader.jsx

--- portfolio/src/components/BoxHeader/BoxHeader.jsx
+++ portfolio/src/components/BoxHeader/BoxHeader.jsx
@@ -3,30 +3,27 @@ import { motion } from "framer-motion";
 const BoxHeader = () => {
   return (
     <motion.div
-      className="bg-opacity-30 bg-white p-6 max-w-[600px] border border-opacity-10 rounded-md text-justify mt-10"
+      className="p-6 w-[900px]  text-justify mt-10 "
       initial={{ opacity: 0.8, scale: 0.9 }}
       transition={{ duration: 1, delay: 0.1 }}
       whileInView={{ scale: 1, opacity: 1 }}
     >
-      <h3 className="text-center text-2xl mb-2">Sobre Mí</h3>
-      <p className="text-start text-sm mb-2">
-        ¡Hola, soy Juan Facundo Tam! Soy un apasionado desarrollador FullStack
-        que decidió dar un giro vocacional desde la psicología hacia la
-        informática para dedicarme a lo que realmente me apasiona: la
-        programación.
-      </p>
-      <p className="text-start text-sm mb-2">
-        Desde entonces, me he mantenido en constante formación, investigando,
-        aprendiendo y acumulando experiencia.
-      </p>
-      <p className="text-start text-sm mb-2">
-        Me considero una persona responsable, perseverante y creativa, y destaco
-        en la resolución de problemas mediante la búsqueda activa de soluciones
-        y alternativas. Tengo una gran apertura, interés y predisposición para
-        trabajar en equipo, ya que considero que el trabajo colaborativo es
-        esencial para el crecimiento tanto profesional como personal. Sigo
-        comprometido con mi crecimiento continuo y estoy emocionado por explorar
-        las oportunidades que la programación me brinda en el futuro.
+      <h3 className="text-center text-[40px] font-bold mb-2">Sobre Mí</h3>
+      <p className="text-center text-xl mb-2">
+        ¡Hola! Soy Nahuel Nicolás Fadel, Desarrollador FullStack.
+        <br />
+        Desde entonces, me he mantenido en constante formación, tanto en
+        experiencia como en conocimientos. Me considero una persona comprometida
+        con mi crecimiento continuo Tengo un enfoque dedicado en mejorar mis
+        habilidades y aplicar mis conocimientos en proyectos interesantes y
+        desafiantes. Estoy emocionado por seguir explorando nuevas tecnologías y
+        oportunidades que la programación me brinda en el futuro.
+        <br /> Me destaco en la resolución de problemas mediante la búsqueda
+        activa de soluciones y alternativas, y en resolver diferente problemas
+        de manera creativa. Ademas estoy super dispuesto a mejorar, aprender mas
+        sobre esta profesión y contribuir con muchos proyectos que me ayuden a
+        formarme como profesional. Valoro mucho el trabajo en equipo y la
+        interaccion constante!
       </p>
     </motion.div>
   );
