2a910c3fe8ae919ba2f8d3fcc191c0fa75fecffc
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-29 17:57:57 -0300

    Update Home.jsx

--- portfolio/src/views/Home/Home.jsx
+++ portfolio/src/views/Home/Home.jsx
@@ -4,11 +4,12 @@ import Social from "../../components/Social/Social";
 import BoxHeader from "../../components/BoxHeader/BoxHeader";
 import { motion } from "framer-motion";
 import Skills from "../../components/Skills/Skills";
+import Title from "../../components/Title/Title";
 
 const Home = () => {
   return (
     <div
-      className="flex flex-col items-center justify-start pt-40 w-full px-40"
+      className="flex flex-col items-center justify-start pt-40 w-full px-40 mb-10 "
       id="home"
     >
       <div className="flex justify-between w-full items-center">
@@ -19,26 +20,11 @@ const Home = () => {
             initial={{ opacity: 0, scale: 0.9 }}
             whileInView={{ scale: 1, opacity: 1 }}
             transition={{ duration: 1 }}
-            className="w-[700px] rounded-[15px]  mt-5  shadow-sm shadow-purple-950 mx-auto"
+            className="w-[600px] rounded-[15px]  mt-5  shadow-sm shadow-purple-950 mx-auto"
           />
         </div>
         <div className="flex flex-col justify-between h-full">
-          <motion.h1
-            initial={{ opacity: 0.8, y: 200 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="text-4xl font-bold"
-          >
-            Nahuel Nicolas Fadel
-          </motion.h1>
-          <motion.h2
-            initial={{ opacity: 0, y: 200 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1, delay: 1 }}
-            className="text-xl"
-          >
-            FullStack Developer
-          </motion.h2>
+          <Title />
           <BoxHeader />
           <Social />
         </div>
