27214fbe25607ee1c5b2a89eab75da4564ad37e4
Nikitoo17 <nicolasfadel17@gmail.com>
2023-09-04 18:45:15 -0300

    Update Skills.jsx

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -25,6 +25,27 @@ import {
 } from "react-icons/si";
 
 const Skills = () => {
+  const skills = [
+    { icon: SiJavascript, text: "Javascript" },
+    { icon: SiTypescript, text: "Typescript" },
+    { icon: SiReact, text: "React" },
+    { icon: SiNextdotjs, text: "NextJS" },
+    { icon: SiVite, text: "Vite" },
+    { icon: SiRedux, text: "Redux" },
+    { icon: SiNodedotjs, text: "NodeJS" },
+    { icon: SiExpress, text: "Express" },
+    { icon: SiPostgresql, text: "PostgreSQL" },
+    { icon: SiSequelize, text: "Sequelize" },
+    { icon: SiMongodb, text: "MongoDB" },
+    { icon: SiTailwindcss, text: "Tailwind" },
+    { icon: SiCss3, text: "CSS" },
+    { icon: SiHtml5, text: "HTML" },
+    { icon: SiSass, text: "Sass" },
+    { icon: SiLess, text: "Less" },
+    { icon: SiCplusplus, text: "C++" },
+    { icon: SiCsharp, text: "C#" },
+    { icon: SiDotnet, text: ".NET" },
+  ];
   return (
     <div className={style.contenedor} id="skills">
       <motion.div
@@ -35,7 +56,13 @@ const Skills = () => {
       >
         <h1 className={style.title}>My Skills</h1>
         <motion.div className={style.divIcons}>
-          <div className={style.divSkill}>
+          skills && skills.map((item, index) => (
+          <div className={style.divSkill} key={index}>
+            {React.createElement(item.icon, { className: style.icons })}
+            <span>{item.text}</span>
+          </div>
+          ))
+          {/* <div className={style.divSkill}>
             <SiJavascript className={style.icons} />
             <span>Javascript</span>
           </div>
@@ -110,7 +137,7 @@ const Skills = () => {
           <div className={style.divSkill}>
             <SiDotnet className={style.icons} />
             <span>.Net</span>
-          </div>
+          </div> */}
         </motion.div>
       </motion.div>
     </div>
