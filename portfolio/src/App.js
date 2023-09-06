b856d83fd004bbaa72078dd43fa237dd1ca878e6
Nikitoo17 <nicolasfadel17@gmail.com>
2023-09-06 14:57:11 -0300

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,14 +3,18 @@ import Proyects from "./views/Proyects/Proyects";
 import Contact from "./views/Contact/Contact";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-black via-black  to-purple-950 text-center scroll-smooth">
       <div className="min-h-screen gap-y-11 flex flex-col items-center justify-center text-white font-poppins">
         <Nav />
-        <Home />
-        <Proyects />
+        <div className="px-40">
+          <Home />
+          <Skills />
+          <Proyects />
+        </div>
         <Contact />
         {/* <Footer /> */}
       </div>
